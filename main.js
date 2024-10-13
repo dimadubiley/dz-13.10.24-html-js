4eb595ba82d4ae9c9b656e4277f5f1584e73fcbe
MisterNP <dimon.pr.2007@gmail.com>
2024-10-13 20:45:46 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let div = document.querySelectorAll("div");
+for (const b of div)
+{
+    b.style.background = ("aqua");
+}
+let h = document.querySelectorAll("h1");
+for (const b of h)
+{
+    b.style.display = ("flex");
+    b.style.fontSize = ("30px");
+}
